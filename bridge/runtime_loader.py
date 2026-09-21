0b9d47062177d539254d0f0614b7c2771ffe43be
punzer4-code <punzer4@gmail.com>
2026-09-21 08:02:04 +0700

    refactor: remove begin operation override allowance

--- bridge/runtime_loader.py
+++ bridge/runtime_loader.py
@@ -45,7 +45,6 @@ DEFAULT_RUNTIME_STAGES = (
         ((
             "recovery.py",
             (
-                "begin_operation",
                 "regenerate_last",
                 "continue_last",
                 "regenerate_edited_turn",
