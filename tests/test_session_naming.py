eb2336e7d7117a750fa5db9b1978777db775b586
punzer4-code <punzer4@gmail.com>
2026-09-21 14:04:02 +0700

    test: retire runtime database fixture state

--- tests/test_session_naming.py
+++ tests/test_session_naming.py
@@ -3,16 +3,15 @@ import json
 import tempfile
 import unittest
 
+import bridge.config as config
 import bridge.runtime as rt
 
 
 class SessionNamingTests(unittest.TestCase):
     def setUp(self):
         self.tmp = tempfile.TemporaryDirectory()
-        self.old_db = rt.DB_FILE
-        rt.DB_FILE = Path(self.tmp.name) / "bridge.sqlite3"
-        with rt._DB_SCHEMA_LOCK:
-            rt._DB_SCHEMA_READY = False
+        self.old_db = config.DB_FILE
+        config.DB_FILE = Path(self.tmp.name) / "bridge.sqlite3"
         self.db = rt.db_connect()
         self.session = rt.ensure_session(self.db, "chat", rt.DEFAULT_MODEL)
         self.sent = []
@@ -22,9 +21,7 @@ class SessionNamingTests(unittest.TestCase):
     def tearDown(self):
         rt.send_text = self.old_send
         self.db.close()
-        rt.DB_FILE = self.old_db
-        with rt._DB_SCHEMA_LOCK:
-            rt._DB_SCHEMA_READY = False
+        config.DB_FILE = self.old_db
         self.tmp.cleanup()
 
     def _state(self, chat_id="chat"):
