0f971b74424554bdd64521542b0238d307091a11
punzer4-code <punzer4@gmail.com>
2026-09-22 13:22:00 +0700

    test: use complete application service fixtures

--- tests/test_group_turn_gating.py
+++ tests/test_group_turn_gating.py
@@ -1,4 +1,4 @@
-from application_test_setup import ensure_application_extensions, make_test_memory_service, make_test_persona_service
+from application_test_setup import ensure_application_extensions, make_test_application_services, make_test_memory_service, make_test_persona_service
 
 ensure_application_extensions()
 
